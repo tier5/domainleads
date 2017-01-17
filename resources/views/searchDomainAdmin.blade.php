9e83371d96bec89cdd9619dd7b10b4b0f7c650b8
tier5 <github@tier5.us>
2017-01-17 12:10:22 +0530

    a

--- resources/views/searchDomainAdmin.blade.php
+++ resources/views/searchDomainAdmin.blade.php
@@ -44,34 +44,38 @@
 	<div class="container">
 	<?php 
 
-	$curl = curl_init();
+	$ch = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://www.textinbulk.com/app/api/validate-us-phone-number",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"phone_number\"\r\n\r\n8123904629\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
-  CURLOPT_HTTPHEADER => array(
-    "cache-control: no-cache",
-    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-    "postman-token: 5bf6235b-4272-6534-c66d-2d989c06652f"
-  ),
-));
+	curl_setopt($ch, CURLOPT_URL, "https://textinbulk.com/app/api/validate-us-phone-number");
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	  
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+	$data = array(
+	    'phone_number' => '8123904629'
+	   
+	);
 
-curl_close($curl);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+	$contents = curl_exec($ch);
+    $err = curl_error($ch);
+	curl_close($ch);
+	//$json = json_decode($contents, true);
+	//print_r($json['http_code']);
+	//$response = curl_exec($curl);
+	
+
+	//curl_close($curl);
+
+	if ($err) {
+	  echo "cURL Error #:" . $err;
+	} else {
+	  echo $contents;
+	}
+
 
 
 
