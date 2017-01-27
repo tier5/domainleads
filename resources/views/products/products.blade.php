a1b30002aa3ed01b8752c205f16ae97f787fb9f7
Tier5 <github@tier5.com>
2017-01-27 13:37:19 +0530

    updating server after removing unnecessory filesss v1

--- resources/views/products/products.blade.php
+++
@@ -1,5 +0,0 @@
-@foreach($products as $product)
-	<h3>{{$product->name}}</h3>
-@endforeach
-
-{{$products->links()}}
--- resources/views/user_profile.blade.php
+++
@@ -1,6 +0,0 @@
-this user profile page {!! Html::link('mylogout','click here to logout')!!}
-@if(Auth::check())
-  Th user is logged in...
-  {{  Auth::id()}}
-  	{{  Auth::user()->email}}
-@endif
